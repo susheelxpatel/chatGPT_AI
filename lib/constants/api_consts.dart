9d46a288cf750d62b7f33fc59e83f7ad40f8a39f
Susheel Patel <susheel.patel@outlook.com>
2024-01-15 17:48:10 +0530

    ChatGPT

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/services/api_service.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import 'package:chatgpt_course/constants/api_consts.dart';
+import 'package:chatgpt_course/models/chat_model.dart';
+import 'package:chatgpt_course/models/models_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static Future<List<ModelsModel>> getModels() async {
+    try {
+      var response = await http.get(
+        Uri.parse("$BASE_URL/models"),
+        headers: {'Authorization': 'Bearer $API_KEY'},
+      );
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+
+      List temp = [];
+      for (var value in jsonResponse["data"]) {
+        temp.add(value);
+
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
+    } catch (error) {
+      log("error $error");
+      rethrow;
+    }
+  }
+
+
+  static Future<List<ChatModel>> sendMessageGPT(
+      {required String message, required String modelId}) async {
+    try {
+      log("modelId $modelId");
+      var response = await http.post(
+        Uri.parse("$BASE_URL/chat/completions"),
+        headers: {
+          'Authorization': 'Bearer $API_KEY',
+          "Content-Type": "application/json"
+        },
+        body: jsonEncode(
+          {
+            "model": modelId,
+            "messages": [
+              {
+                "role": "user",
+                "content": message,
+              }
+            ]
+          },
+        ),
+      );
+
+
+      Map jsonResponse = json.decode(utf8.decode(response.bodyBytes));
+      if (jsonResponse['error'] != null) {
+
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse["choices"].length > 0) {
+
+        chatList = List.generate(
+          jsonResponse["choices"].length,
+              (index) => ChatModel(
+            msg: jsonResponse["choices"][index]["message"]["content"],
+            chatIndex: 1,
+          ),
+        );
+      }
+      return chatList;
+    } catch (error) {
+      log("error $error");
+      rethrow;
+    }
+  }
+
+
+  static Future<List<ChatModel>> sendMessage(
+      {required String message, required String modelId}) async {
+    try {
+      log("modelId $modelId");
+      var response = await http.post(
+        Uri.parse("$BASE_URL/completions"),
+        headers: {
+          'Authorization': 'Bearer $API_KEY',
+          "Content-Type": "application/json"
+        },
+        body: jsonEncode(
+          {
+            "model": modelId,
+            "prompt": message,
+            "max_tokens": 300,
+          },
+        ),
+      );
+
+
+
+      Map jsonResponse = json.decode(utf8.decode(response.bodyBytes));
+      if (jsonResponse['error'] != null) {
+
+        throw HttpException(jsonResponse['error']["message"]);
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse["choices"].length > 0) {
+
+        chatList = List.generate(
+          jsonResponse["choices"].length,
+              (index) => ChatModel(
+            msg: jsonResponse["choices"][index]["text"],
+            chatIndex: 1,
+          ),
+        );
+      }
+      return chatList;
+    } catch (error) {
+      log("error $error");
+      rethrow;
+    }
+  }
+}
